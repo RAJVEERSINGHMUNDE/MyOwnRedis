fcc426c35ba6137df2fa4dfc3d693622ce874c87
none <none>
2026-08-17 23:02:51 +0530

    resp

---
+++ src/resp.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+	"bufio"
+	"io"
+)
+
+const (
+	STRING = '+'
+	ERROR = '-'
+	INTEGER = ':'
+	BULK = '$'
+	ARRAY = '*'
+)
+
+type Value struct {
+	typ string
+	str string
+	num int
+	bulk string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func read
+
