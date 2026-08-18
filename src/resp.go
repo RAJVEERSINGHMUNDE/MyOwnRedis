4bb20f71d2a8a38fc2a8742edcd0c129a74eca02
RAJVEER SINGH MUNDE <f20231317@pilani.bits-pilani.ac.in>
2026-08-18 11:38:42 +0530

    resp

--- src/resp.go
+++ src/resp.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"strconv"
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 )
 
 const (
-	STRING = '+'
-	ERROR = '-'
+	STRING  = '+'
+	ERROR   = '-'
 	INTEGER = ':'
-	BULK = '$'
-	ARRAY = '*'
+	BULK    = '$'
+	ARRAY   = '*'
 )
 
 type Value struct {
-	typ string
-	str string
-	num int
-	bulk string
+	typ   string
+	str   string
+	num   int
+	bulk  string
 	array []Value
 }
 
@@ -32,5 +31,50 @@ func NewResp(rd io.Reader) *Resp {
 	return &Resp{reader: bufio.NewReader(rd)}
 }
 
-func read
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line := append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+	return line[:len(line)-2], n, nil
+}
 
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return int(i64), n, nil
+}
+
+func (r *Resp) Read() (Value, error) {
+	_type, err := r.reader.ReadByte()
+	if err != nil {
+		return Value{}, err
+	}
+
+	switch _type {
+	case ARRAY:
+		return r.readArray()
+	case BULK:
+		return r.readBulk()
+	default:
+		fmt.Println("Unknown Value type: %v", string(_type))
+		return Value{}, nil
+	}
+}
+
+func (r *Resp) readArray() (Value, error) {
+
+}
